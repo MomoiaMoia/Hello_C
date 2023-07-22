c3e4257fabd579826faf60f44f5b2da1642bd853
UesugiSaori <1132756648@qq.com>
2023-07-22 21:32:55 +0800

    ch2_3

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
 #include <stdio.h>
 
-int main() {
-	printf("Hello C!");
-	return 0;
+void butler(void);
+int main(void) {
+	printf("* I will call butler;\n");
+	butler();
+	printf("* Finish;\n");
+}
+void butler(void) {
+	printf("\t- This is butler;\n");
 }
